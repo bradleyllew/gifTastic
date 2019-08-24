ea8df672571ef3d76968598b83b18e1549ebc188
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-08-24 16:32:36 -0400

    Spent the week thinking I was crazy bez nothing was working - bombed all out 3 times.  Just found 2 typos that made all the difference! *headsmack* Implemented onclick to grab user input. Need to fix default (have to use button for now?) Added ajax

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,19 @@
 
 
 $(document).ready(function () {
-    createBtn();
+    starterBtn();
 })
 
 // Topic Buttons
-function createBtn() {
+function starterBtn() {
     $("#buttons").empty();
     var topics = [
-        "Captain Caveman ", "Transformers ", "Voltron ", "He-man and the Masters of the Universe "
+        "Captain Caveman ", "Transformers ", "Voltron ", "He-man and the Masters of the Universe ", "Care Bears"
     ];
 
     for (var i = 0; i < topics.length; i++) {
         var btn = $("<button>" + topics[i] + "</button>");
-        btn.addClass("jsonData");
+        btn.addClass("initialButtons");
         btn.attr("data-name", topics[i]);
         btn.attr("onclick", "displayGifs('" + topics[i] + "')");
         btn.appendTo("#buttons");
@@ -26,7 +26,7 @@ function displayGifs(topic) {
         topic +
         "&limit=10&api_key=gQZBZ5VqEfopGgEePg03EipzZzHrwAlo";
     var apiKey = "gQZBZ5VqEfopGgEePg03EipzZzHrwAlo";
-    // var p = newFunction();
+
 
     $.ajax({
         url: queryUrl,
@@ -38,19 +38,45 @@ function displayGifs(topic) {
                 var img = $("<img>");
                 var imgUrl = result[i].images.original.url;
                 img.attr("src", imgUrl);
-                $("#gifs").append(img);
-                // $("#gifs").append(p);
+                img.attr("alt", "cartoon image");
+                $("#gifs").prepend(img);
+
             }
         }
     });
-
-
-    // function newFunction() {
-    //     var rating = response.data.rating;
-    //     var p = $("<p>").text("Rating: " + rating);
-    //     return p;
-    // }
 }
+// Function to make new buttons generated from user search
+$("#find-cartoon").on("click", function (event) {
+    // Bez yall said to do this each time :)
+    event.preventDefault();
+
+    var cartoon = $("#inputDefault").val();
+    console.log(cartoon);
+    var queryUrl =
+        "https://api.giphy.com/v1/gifs/search?q=" +
+        cartoon +
+        "&limit=10&api_key=gQZBZ5VqEfopGgEePg03EipzZzHrwAlo";
+
+    // to call ajax and display gifs in the 'gifs' div
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        // $("#gifs").text(JSON.stringify(response));
+        if (response.data.length > 1) {
+            for (var i = 1; i < 9; i++) {
+                var result = response.data;
+                var img = $("<img>");
+                var imgUrl = result[i].images.original.url;
+                img.attr("src", imgUrl);
+                img.attr("alt", "cartoon image");
+                $("#gifs").prepend(img);
+
+            }
+        }
+        
+    });
+})
 
 
 
@@ -66,78 +92,4 @@ function displayGifs(topic) {
 
 
 
-  // function displayCartoonInfo() {
-
-//     var cartoon = $(this).attr("data-name");
-//     var queryURL = url;
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-
-//     }).then(function (response) {
-
-//         // Creating a div to hold the movie
-//         var cartoonDiv = $("<div class='cartoon'>");
-
-//         // Storing the rating data
-//         var rating = response.Rated;
-
-//         // Creating an element to have the rating displayed
-//         var pOne = $("<p>").text("Rating: " + rating);
-
-//         // Displaying the rating
-//         cartoonDiv.append(pOne);
-
-//         var imgURL = response.Poster;
-
-//         // Creating an element to hold the image
-//         var image = $("<img>").attr("src", imgURL);
-
-//         // Appending the image
-//         cartoonDiv.append(image);
-
-//         // Putting the entire movie above the previous movies
-//         $("#buttons-appear-here").prepend(cartoonDiv);
-//     });
-// }
-
-// function renderButtons() {
-
-//     // Deleting the movies prior to adding new movies
-//     // (this is necessary otherwise you will have repeat buttons)
-//     $("#buttons-view").empty();
-
-//     // Looping through the array of movies
-//     for (var i = 0; i < topics.length; i++) {
-
-//       // Then dynamicaly generating buttons for each movie in the array
-//       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-//       var a = $("<button>");
-//       // Adding a class of movie-btn to our button
-//       a.addClass("cartoon-btn");
-//       // Adding a data-attribute
-//       a.attr("data-name", topics[i]);
-//       // Providing the initial button text
-//       a.text(topics[i]);
-//       // Adding the button to the buttons-view div
-//       $("#buttons-view").append(a);
-//     }
-//   }
-//   $("#add-cartoon").on("click", function(event) {
-//     event.preventDefault();
-//     // This line grabs the input from the textbox
-//     var cartoon = $(".btn").val().trim();
-
-//     // Adding movie from the textbox to our array
-//     topics.push(cartoon);
-
-//     // Calling renderButtons which handles the processing of our movie array
-//     renderButtons();
-//   });
-
-//   // Adding a click event listener to all elements with a class of "movie-btn"
-//   $(document).on("click", ".btn", displayCartoonInfo);
-
-//  Calling the renderButtons function to display the intial buttons
 
--- assets/topics.js
+++
@@ -1,4 +0,0 @@
-// var topics = ["Captain Caveman ", "Transformers ", "Voltron ", "He-man and the Masters of the Universe "];
-//     for (var i = 0; i < topics.length; i++) {
-//         console.log("My favorite cartoon is " + topics[i]);
-//     };
